5ebc52718aefc13b680d5f47e9163426644d3892
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-08-12 11:29:22 -0500

    v0.68.32 Se integra codigo

--- src/html_controler.php
+++ src/html_controler.php
@@ -123,6 +123,30 @@ class html_controler{
         return $keys;
     }
 
+    public function input_codigo(int $cols, stdClass $row_upd, bool $value_vacio): array|string
+    {
+
+        if($cols<=0){
+            return $this->error->error(mensaje: 'Error cold debe ser mayor a 0', data: $cols);
+        }
+        if($cols>=13){
+            return $this->error->error(mensaje: 'Error cold debe ser menor o igual a  12', data: $cols);
+        }
+
+        $html =$this->directivas->input_text_required(disable: false,name: 'codigo',place_holder: 'Codigo',row_upd: $row_upd,
+            value_vacio: $value_vacio);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar input', data: $html);
+        }
+
+        $div = $this->directivas->html->div_group(cols: $cols,html:  $html);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al integrar div', data: $div);
+        }
+
+        return $div;
+    }
+
     /**
      * Genera un input de tipo ID
      * @param int $cols
