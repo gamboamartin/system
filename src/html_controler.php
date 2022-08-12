8ebc9bcfa0e81589b834544988cbc6cd9294bee0
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-08-12 11:14:18 -0500

    v0.66.32 Se integra input id

--- src/html_controler.php
+++ src/html_controler.php
@@ -123,6 +123,30 @@ class html_controler{
         return $keys;
     }
 
+    public function input_id(int $cols, stdClass $row_upd, bool $value_vacio, bool $disabled = false): array|string
+    {
+
+        if($cols<=0){
+            return $this->error->error(mensaje: 'Error cold debe ser mayor a 0', data: $cols);
+        }
+        if($cols>=13){
+            return $this->error->error(mensaje: 'Error cold debe ser menor o igual a  12', data: $cols);
+        }
+
+        $html =$this->directivas->input_text_required(disable: $disabled,name: 'id',place_holder: 'Id',row_upd: $row_upd,
+            value_vacio: $value_vacio);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar input', data: $html);
+        }
+
+        $div = $this->directivas->html->div_group(cols: $cols,html:  $html);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al integrar div', data: $div);
+        }
+
+        return $div;
+    }
+
     /**
      * @param stdClass $cols Objeto con la definicion del numero de columnas a integrar en un input base
      * @version 0.11.5
