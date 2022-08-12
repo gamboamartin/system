21cd46eed53bd52f991868cf2dc8ae595fa77731
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-08-12 11:43:10 -0500

    v0.71.32 Se genera ut codigo

--- tests/src/html_controlerTest.php
+++ tests/src/html_controlerTest.php
@@ -103,6 +103,25 @@ class html_controlerTest extends test {
         errores::$error = false;
     }
 
+    public function test_input_codigo(): void
+    {
+        errores::$error = false;
+        $html_ = new html();
+        $html = new html_controler($html_);
+        //$html = new liberator($html);
+
+        $row_upd = new stdClass();
+        $cols = 1;
+        $value_vacio = false;
+
+        $resultado = $html->input_codigo($cols, $row_upd, $value_vacio);
+        $this->assertIsString($resultado);
+        $this->assertNotTrue(errores::$error);
+        $this->assertEquals("<div |class|><div |class|><input type='text' name='codigo' value='' |class|  required id='codigo' placeholder='Codigo' /></div></div>", $resultado);
+
+        errores::$error = false;
+    }
+
     /**
      * @throws JsonException
      */
