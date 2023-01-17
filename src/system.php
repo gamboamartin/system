dcabb26d7c3feedf47b0d0dcf5bd0682852ec07b
mgamboa <gamboa.martin@gmail.com>
2023-01-17 17:56:26 -0600

    v4.53.2 Se implementa simplificacion de inputs upd

--- src/system.php
+++ src/system.php
@@ -610,12 +610,28 @@ class system extends controlador_base{
         }
         $this->forms_inputs_modifica = $form_modifica;
 
-        $include_inputs_modifica = (new generales())->path_base."templates/inputs/$this->seccion/modifica.php";
+        /*$include_inputs_modifica = (new generales())->path_base."templates/inputs/$this->seccion/modifica.php";
         if(!file_exists($include_inputs_modifica)){
             $include_inputs_modifica = (new views())->ruta_templates."inputs/base/modifica.php";
         }
 
+        $this->include_inputs_modifica = $include_inputs_modifica;*/
+
+        /**
+         * REFCATROIZAR SIMILAR ALTA
+         */
+
+        $include_inputs_modifica = (new generales())->path_base."templates/inputs/$this->seccion/modifica.php";
+        if(!file_exists($include_inputs_modifica)){
+            $include_inputs_modifica = (new views())->ruta_templates."inputs/base/modifica.php";
+
+            $path_vendor_base = $this->path_base."vendor/$this->path_vendor_views/templates/inputs/$this->seccion/$this->accion.php";
+            if(file_exists($path_vendor_base)){
+                $include_inputs_modifica = $path_vendor_base;
+            }
+        }
         $this->include_inputs_modifica = $include_inputs_modifica;
+
 
         return $r_modifica;
     }
