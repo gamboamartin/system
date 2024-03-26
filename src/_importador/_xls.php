e1d6ae1f5aaebbe373a03ef4e1e79be87d8467de
mgamboa <gamboa.martin@gmail.com>
2024-03-26 16:45:41 -0600

    v20.16.0 Se integra funcionalidad campos no required

--- src/_importador/_xls.php
+++ src/_importador/_xls.php
@@ -5,6 +5,7 @@ use gamboamartin\errores\errores;
 use gamboamartin\plugins\Importador;
 use gamboamartin\system\html_controler;
 use PDO;
+use stdClass;
 
 class _xls
 {
@@ -14,6 +15,37 @@ class _xls
     public function __construct()
     {
         $this->error = new errores();
+
+    }
+
+    private function adm_campos_limpios(array $adm_campos, array $columnas_doc)
+    {
+        $no_obligatorios = array('id');
+        foreach ($adm_campos as $indice=>$adm_campo){
+
+            $adm_campos = $this->limpia_campo_opcional(adm_campo: $adm_campo,adm_campos:  $adm_campos,
+                columnas_doc:  $columnas_doc,indice:  $indice,no_obligatorios:  $no_obligatorios);
+            if(errores::$error){
+                return $this->error->error(mensaje: 'Error al limpiar adm_campos',data:  $adm_campos);
+            }
+        }
+        return $adm_campos;
+
+    }
+
+
+    private function adm_campos_inputs(array $columnas_doc, PDO $link, string $tabla)
+    {
+        $adm_campos = (new _campos())->adm_campos(link: $link, tabla: $tabla);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al obtener adm_campos',data:  $adm_campos);
+        }
+
+        $adm_campos = $this->adm_campos_limpios(adm_campos: $adm_campos,columnas_doc:  $columnas_doc);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al limpiar adm_campos',data:  $adm_campos);
+        }
+        return $adm_campos;
 
     }
 
@@ -62,19 +94,34 @@ class _xls
 
     final public function columnas_xls(string $ruta, html_controler $html_controler,PDO $link, string $tabla): array
     {
-        $columnas_calc_def = $this->genera_columnas_calc_def(ruta: $ruta);
+        $datos_xls = $this->genera_columnas_calc_def(ruta: $ruta);
         if(errores::$error){
-            return $this->error->error(mensaje: 'Error al integrar columna',data:  $columnas_calc_def);
+            return $this->error->error(mensaje: 'Error al integrar columna',data:  $datos_xls);
         }
 
-        $columnas_xls = $this->genera_inputs_importa(columnas_calc_def: $columnas_calc_def,html_controler: $html_controler,link: $link,tabla: $tabla);
+        $columnas_xls = $this->genera_inputs_importa(
+            columnas_calc_def: $datos_xls->columnas_calc_def, columnas_doc: $datos_xls->columnas_calc,
+            html_controler: $html_controler, link: $link, tabla: $tabla);
         if(errores::$error){
             return $this->error->error(mensaje: 'Error al generar columnas_xls', data: $columnas_xls);
         }
         return $columnas_xls;
     }
 
-    private function genera_columnas_calc_def(string $ruta): array
+    private function existe_campo(array $adm_campo, array $columnas_doc): bool
+    {
+        $existe_campo = false;
+        foreach ($columnas_doc as $columna_doc){
+            if($adm_campo['adm_campo_descripcion']===$columna_doc){
+                $existe_campo = true;
+                break;
+            }
+        }
+        return $existe_campo;
+
+    }
+
+    private function genera_columnas_calc_def(string $ruta): array|stdClass
     {
         $columnas_calc = (new Importador())->primer_row(celda_inicio: 'A1',ruta_absoluta: $ruta);
         if(errores::$error){
@@ -85,18 +132,24 @@ class _xls
         if(errores::$error){
             return $this->error->error(mensaje: 'Error al integrar columna',data:  $columnas_calc_def);
         }
-        return $columnas_calc_def;
+
+        $datos = new stdClass();
+        $datos->columnas_calc = $columnas_calc;
+        $datos->columnas_calc_def = $columnas_calc_def;
+        return $datos;
 
     }
 
-    private function genera_inputs_importa(array $columnas_calc_def, html_controler $html_controler, PDO $link, string $tabla): array
+    private function genera_inputs_importa(array $columnas_calc_def, array $columnas_doc, html_controler $html_controler, PDO $link, string $tabla): array
     {
-        $adm_campos = (new _campos())->adm_campos(link: $link, tabla: $tabla);
+        $adm_campos = $this->adm_campos_inputs(columnas_doc: $columnas_doc,link:  $link,tabla:  $tabla);
+
         if(errores::$error){
-            return $this->error->error(mensaje: 'Error al obtener adm_campos',data:  $adm_campos);
+            return $this->error->error(mensaje: 'Error al limpiar adm_campos',data:  $adm_campos);
         }
 
-        $columnas_xls = $this->inputs_selects_importa(adm_campos: $adm_campos, columnas_calc_def: $columnas_calc_def, html_controler: $html_controler, link: $link);
+        $columnas_xls = $this->inputs_selects_importa(adm_campos: $adm_campos, columnas_calc_def: $columnas_calc_def,
+            html_controler: $html_controler, link: $link);
         if(errores::$error){
             return $this->error->error(mensaje: 'Error al generar columnas_xls', data: $columnas_xls);
         }
@@ -134,6 +187,21 @@ class _xls
         $columnas_calc_def[] = $columna_calc_def;
 
         return $columnas_calc_def;
+
+    }
+
+    private function limpia_campo_opcional(array $adm_campo, array $adm_campos, array $columnas_doc, int $indice, array $no_obligatorios)
+    {
+        $existe_campo = $this->existe_campo(adm_campo: $adm_campo,columnas_doc:  $columnas_doc);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al validar si existe campo',data:  $existe_campo);
+        }
+        if(!$existe_campo){
+            if(in_array($adm_campo['adm_campo_descripcion'], $no_obligatorios)){
+                unset($adm_campos[$indice]);
+            }
+        }
+        return $adm_campos;
 
     }
 
