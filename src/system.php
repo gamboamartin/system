5be481b959b25853d78ce668fffd99c3265a6fff
mgamboa <gamboa.martin@gmail.com>
2024-04-02 14:45:17 -0600

    v20.28.0 Se integra importa controller

--- src/system.php
+++ src/system.php
@@ -997,6 +997,59 @@ class system extends controlador_base{
         return $this->inputs;
     }
 
+    final public function importa_previo_muestra_bd(bool $header = true, bool $ws = false): array|stdClass
+    {
+
+        $doc_documento = $this->modelo_doc_documento->registro(registro_id: $_GET['doc_documento_id'],
+            columnas_en_bruto: true, retorno_obj: true);
+        if(errores::$error){
+            return $this->retorno_error(mensaje: 'Error al obtener documento',data:  $doc_documento,
+                header: $header,ws:  $ws);
+        }
+        $rs = (new _importa())->importa_registros_xls(modelo: $this->modelo,
+            ruta_absoluta: $doc_documento->ruta_absoluta);
+        if(errores::$error){
+            return $this->retorno_error(mensaje: 'Error al inserta registro', data: $rs, header: $header,
+                ws: $ws, class: __CLASS__, file: __FILE__, function: __FUNCTION__, line: __LINE__);
+        }
+        $data_rs = serialize($rs);
+        $data_rs = base64_encode($data_rs);
+        $_SESSION['rs_importa'] = $data_rs;
+
+
+        $rs->registro_id = -1;
+        $out = $this->out_alta(header: $header,id_retorno:  -1,r_alta_bd:  $rs,
+            seccion_retorno:  $this->seccion,siguiente_view:  'importa_result',ws:  $ws);
+        if(errores::$error){
+            print_r($out);
+            die('Error');
+        }
+
+        return $rs;
+    }
+
+    final public function importa_result(bool $header = true, bool $ws = false): array|stdClass{
+        $rs_importa = $_SESSION['rs_importa'];
+        $rs_importa = base64_decode($rs_importa);
+        $rs_importa = unserialize($rs_importa);
+
+        $datos = $rs_importa->datos;
+
+        $columnas_xls = $datos->columns;
+        $rows_xls = $datos->rows;
+
+        $this->registros['columnas_xls'] = $columnas_xls;
+        $this->registros['rows_xls'] = $rows_xls;
+        $this->registros['rows_a_importar_db'] = $rs_importa->rows_a_importar_db;
+        $this->registros['transacciones'] = $rs_importa->altas;
+
+
+        //print_r($this->registros['transacciones']);exit;
+
+        return $this->registros;
+
+    }
+
     /**
      * Obtiene los includes de templates alta
      * @return array|string
